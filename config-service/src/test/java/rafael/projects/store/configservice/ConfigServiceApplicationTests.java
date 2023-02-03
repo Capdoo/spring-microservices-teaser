526a6a8d71c7c1f396a2f28187fe6d09daf45e89
Capdoo <mazher_2014@hotmail.com>
2023-02-03 11:38:06 -0500

    props config data

---
+++ config-service/src/test/java/rafael/projects/store/configservice/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package rafael.projects.store.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
