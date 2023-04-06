f1f6898a46139c667bd57b2e271c9da46ad795fc
Capdoo <mazher_2014@hotmail.com>
2023-04-06 16:18:23 -0500

    update config data - hystrix 3

--- ms-shopping-service/src/main/java/rafael/projects/store/springshoppingservice/shoppingservice/ShoppingServiceApplication.java
+++ ms-shopping-service/src/main/java/rafael/projects/store/springshoppingservice/shoppingservice/ShoppingServiceApplication.java
@@ -3,11 +3,16 @@ package rafael.projects.store.springshoppingservice.shoppingservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
+//For Hystrix
+@EnableHystrixDashboard
+@EnableHystrix
 public class ShoppingServiceApplication {
 
 	public static void main(String[] args) {
